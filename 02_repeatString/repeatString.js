2c3854f726f43b7742326a97a63191301caf43a3
Nicolas Rodriguez <94021624+NicRodri@users.noreply.github.com>
2022-04-01 10:27:41 -0700

    Finish exercise 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeats) {
+    let str = "";
+    for(let i = 0; i< repeats; i++){
+        str = str + word;
+    }
+    if (repeats <0){
+        return "ERROR";
+    }
+    return str;
 };
 
 // Do not edit below this line
