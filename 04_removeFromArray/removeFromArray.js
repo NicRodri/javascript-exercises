0b7ba5832a8dfe1da4566cb53f017497b8910427
Nicolas Rodriguez <94021624+NicRodri@users.noreply.github.com>
2022-04-01 13:46:20 -0700

    Finish 04 exercise remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...removes) {
+    let arr = array;
+    for (let i =0; i< removes.length; i++){
+        if (arr.includes(removes[i])){
+            arr.splice(arr.indexOf(removes[i]),1);
+        }
+    }
+    return arr;
+}
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
