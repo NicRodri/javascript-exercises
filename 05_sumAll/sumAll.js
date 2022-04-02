8e0c82f22103b34f01d6fe012161871108002b7c
Nicolas Rodriguez <94021624+NicRodri@users.noreply.github.com>
2022-04-01 19:41:13 -0700

    Finish 05 exercise sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 <0 || num2 <0){
+        return "ERROR";
+    }
+    else if (typeof num1 != "number"|| typeof num2 != "number"){
+        return "ERROR";
+    }
 
-};
+    let sum =0;
+    for (let i=0;i<=Math.abs(num1 - num2); i++){
+        if(sum == 0 && num1<num2){
+            sum = num1;
+        }
+        else if (sum == 0 && num1>num2) {
+            sum = num2;
+        }
+        else if(sum>0 && num1<num2){
+            sum= sum + (num1 + i);
+        }
+        else if(sum>0 && num1>num2){
+            sum = sum + (num2 +i);
+        }
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
