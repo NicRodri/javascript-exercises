3008699634732bffdc95e0e049486a3f703f0531
Nicolas Rodriguez <94021624+NicRodri@users.noreply.github.com>
2022-04-01 20:13:15 -0700

    Finish exercise 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+    let tempC = (tempF-32)*(5/9);
+    let roundedTempC = Math.round(tempC*10)/10;
+    return roundedTempC;
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+    let tempF = (tempC*1.8) +32;
+    let roundedTempF = Math.round(tempF*10)/10;
+    return roundedTempF;
 };
 
 // Do not edit below this line
