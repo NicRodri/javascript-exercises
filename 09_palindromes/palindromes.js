32c4d2408d7f3b9054aff3918a24ae43f8041ef2
Nicolas Rodriguez <94021624+NicRodri@users.noreply.github.com>
2022-04-25 15:01:58 -0700

    finish exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    let splitString = a.split("");
+    function letter(char){
+        let letter = ['.', '?' , '!', ' ', ','];
+        for(let i =0; i<letter.length; i++){
+            if(letter[i]== char){
+                return false;
+            }
+        }
+        return true;
+    }
+    let onlyLetters = splitString.filter(letter).join("").toLowerCase(); 
+    function reverseString(str){
+        if(str === ""){
+            return "";
+        }
+        else {
+            return reverseString(str.substr(1)) + str.charAt(0);
+        }
+    }
+    return (onlyLetters == reverseString(onlyLetters));
+   
+    
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
