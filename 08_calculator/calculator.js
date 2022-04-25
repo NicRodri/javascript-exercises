11903a410c9f936726dabab84a2029c01fe8500f
Nicolas Rodriguez <94021624+NicRodri@users.noreply.github.com>
2022-04-25 13:47:22 -0700

    exercise complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let sumation = 0;
+	if(a == []){
+    return sumation;
+  }
+  else{
+    for(let i =0; i< a.length ; i++){
+      sumation += a[i];
+    }
+    return sumation;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let multiplication = 1;
+  for(let i =0; i< a.length ; i++){
+    multiplication *= a[i];
+  }
+  return multiplication;
 };
 
-const power = function() {
-	
+const power = function(a, n) {
+  let exp = 1;
+	for (let i = 0; i< n; i++){
+    exp *= a;
+  }
+  return exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  if(a == 0){
+    return fact;
+  }
+  for (let i = 0; i< a; i++){
+    fact *= (a - i);
+  }
+  return fact;
 };
 
 // Do not edit below this line
@@ -31,3 +55,4 @@ module.exports = {
   power,
   factorial
 };
+
