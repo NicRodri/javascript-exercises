e28530cbac83c1c9c801fa52aa8a805df2331cbf
Nicolas Rodriguez <94021624+NicRodri@users.noreply.github.com>
2022-04-26 10:13:20 -0700

    finish exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
 
+    let fib = 0;
+    let fibn1 = 1;
+    let fibn2 = 1;
+
+    if(a == 1 || a == 2){
+        return 1;
+    }
+
+    if(a < 0){
+        return "OOPS";
+    }
+
+
+    for(let i=2; i<Number(a); i++){
+        fib = fibn1 + fibn2;
+        fibn2 = fibn1;
+        fibn1 = fib;
+    }
+    return fib;
 };
 
 // Do not edit below this line
